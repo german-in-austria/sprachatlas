185703cfbe6c3de14bfe260bba006e92b8717af4
Jakob Bal <baljakob9@gmail.com>
2021-10-20 14:21:14 +0200

    Added component LegendItem

---
+++ src/components/LegendItem.vue
@@ -0,0 +1,9 @@
+<template></template>
+<script src="ts">
+import Component from "vue-class-component";
+
+@Component({
+  name: "LegendItem",
+})
+export default class LegendItem extends Vue {}
+</script>
