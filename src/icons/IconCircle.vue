f59c4243a29133f2ca99954aad37277eac5ef08b
Jakob Bal <baljakob9@gmail.com>
2021-10-20 17:29:22 +0200

    Added IconCircle

---
+++ src/icons/IconCircle.vue
@@ -0,0 +1,32 @@
+<template>
+  <svg xmlns="http://www.w3.org/2000/svg" :width="width" :height="height">
+    <circle
+      :cx="width / 2"
+      :cy="height / 2"
+      :r="radius"
+      :stroke="strokeColor"
+      :stroke-width="strokeWidth"
+      :fill="fillCol"
+    />
+  </svg>
+</template>
+<script lang="ts">
+import Vue from "vue";
+import { Component, Prop } from "vue-property-decorator";
+
+@Component({
+  name: "IconCircle",
+})
+export default class IconCircle extends Vue {
+  @Prop([Number, String]) readonly radius: number | string | undefined;
+  @Prop({ type: String, default: "black" }) readonly strokeColor!: string;
+  @Prop({ type: [Number, String], default: 18 }) readonly width!:
+    | number
+    | string;
+  @Prop({ type: [Number, String], default: 18 }) readonly height!:
+    | number
+    | string;
+  @Prop({ type: Number, default: 1 }) readonly strokeWidth!: number;
+  @Prop({ type: String, default: "black" }) readonly fillCol!: string;
+}
+</script>
