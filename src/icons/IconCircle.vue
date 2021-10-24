26e7006b0bd5a4c76df4580817d272f21987165f
Jakob Bal <baljakob9@gmail.com>
2021-10-24 18:52:03 +0200

    Improved circle for better display

--- src/icons/IconCircle.vue
+++ src/icons/IconCircle.vue
@@ -1,8 +1,11 @@
 <template>
-  <svg xmlns="http://www.w3.org/2000/svg" :width="width" :height="height">
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    :viewbox="'0 0 ' + width + ' ' + height"
+  >
     <circle
-      :cx="width / 2"
-      :cy="height / 2"
+      cx="50%"
+      cy="50%"
       :r="radius"
       :stroke="strokeColor"
       :stroke-width="strokeWidth"
@@ -18,15 +21,19 @@ import { Component, Prop } from "vue-property-decorator";
   name: "IconCircle",
 })
 export default class IconCircle extends Vue {
-  @Prop([Number, String]) readonly radius: number | string | undefined;
+  @Prop({ type: [Number, String], default: 0 }) readonly radius:
+    | number
+    | string
+    | undefined;
   @Prop({ type: String, default: "black" }) readonly strokeColor!: string;
-  @Prop({ type: [Number, String], default: 18 }) readonly width!:
-    | number
-    | string;
-  @Prop({ type: [Number, String], default: 18 }) readonly height!:
-    | number
-    | string;
   @Prop({ type: Number, default: 1 }) readonly strokeWidth!: number;
   @Prop({ type: String, default: "black" }) readonly fillCol!: string;
+  height: number = 0;
+  width: number = 0;
+
+  beforeCreate() {
+    this.width = (Number(this.radius) + this.strokeWidth) * 2;
+    this.height = this.width;
+  }
 }
 </script>
