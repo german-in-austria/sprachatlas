09e631b8a9c9d412b25de9ea171dd4c9f5df27f2
Jakob Bal <baljakob9@gmail.com>
2021-10-25 10:36:16 +0200

    Added IconBase

---
+++ src/icons/IconBase.vue
@@ -0,0 +1,32 @@
+<template>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    :width="width"
+    :height="height"
+    viewBox="0 0 18 18"
+    :aria-labelledby="iconName"
+    role="presentation"
+  >
+    <g :fill="iconColor">
+      <slot />
+    </g>
+  </svg>
+</template>
+<script lang="ts">
+import Component from "vue-class-component";
+import { Vue, Prop } from "vue-property-decorator";
+
+@Component({
+  name: "IconBase",
+})
+export default class IconBase extends Vue {
+  @Prop({ default: "box" }) readonly iconName!: string;
+  @Prop({ type: [Number, String], default: 18 }) readonly width!:
+    | number
+    | string;
+  @Prop({ type: [Number, String], default: 18 }) readonly height!:
+    | number
+    | string;
+  @Prop({ default: "currentColor" }) readonly iconColor!: string;
+}
+</script>
