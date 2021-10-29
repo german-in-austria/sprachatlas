0be382a04850142f8b8bd61740b661d480f8fa3c
Jakob Bal <baljakob9@gmail.com>
2021-10-29 15:39:36 +0200

    OpenAPI Update

--- src/api/dioe-public-api/core/OpenAPI.ts
+++ src/api/dioe-public-api/core/OpenAPI.ts
@@ -15,7 +15,7 @@ type Config = {
 }
 
 export const OpenAPI: Config = {
-    BASE: '/',
+    BASE: process.env.VUE_APP_API_ENDPOINT,
     VERSION: '1.0.0',
     WITH_CREDENTIALS: false,
     TOKEN: undefined,
