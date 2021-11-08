4795386664b9f60db2dfd8bf4120dc7308883a58
Jakob Bal <baljakob9@gmail.com>
2021-11-08 13:08:46 +0100

    add id generator

--- src/helpers/helper.ts
+++ src/helpers/helper.ts
@@ -8,3 +8,7 @@ export const getOrtName = (name: string) => {
     }
     return {name};
 }
+
+export const generateID = () => {
+    return (Date.now().toString(36) + Math.random().toString(36).substr(2, 5)).toUpperCase();
+}
