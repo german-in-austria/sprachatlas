596b3733aedf46288810b066520ed0acab317cef
Jakob Bal <baljakob9@gmail.com>
2022-01-25 12:45:57 +0100

    Added geojson von vbg

--- src/store/geo.ts
+++ src/store/geo.ts
@@ -4,14 +4,23 @@ import * as _ from 'lodash'
 export const geoStore = {
   bundeslaender: null as geojson.FeatureCollection | null,
   dialektregionen: null as geojson.FeatureCollection | null,
-  gemeinden: null as geojson.FeatureCollection|null,
+  gemeinden: null as geojson.FeatureCollection | null,
   test: null as geojson.FeatureCollection | null
 }
 
 async function init () {
   console.log('Called geo Init function')
   geoStore.bundeslaender = await (await fetch('geojson/bundeslaender.geojson.json')).json() as geojson.FeatureCollection
-  geoStore.dialektregionen = await (await fetch('geojson/SFB_Dialektregionen.geojson')).json() as geojson.FeatureCollection
+  const sfb = await (await fetch('geojson/SFB_Dialektregionen.geojson')).json() as geojson.FeatureCollection;
+  const vlbg = await (await fetch('geojson/vbg.geojson')).json() as geojson.FeatureCollection;
+  geoStore.dialektregionen = concatGeoJSON(sfb, vlbg) as geojson.FeatureCollection;
+}
+
+function concatGeoJSON(g1: any, g2: any) {
+  return { 
+        "type" : "FeatureCollection",
+        "features": [... g1.features, ... g2.features]
+    }
 }
 
 export const initialize = _.once(init)
