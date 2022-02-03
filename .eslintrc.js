78be3889ddc5928ea5df12d5e9be9cfd117bb04a
Jakob Bal <baljakob9@gmail.com>
2022-02-03 15:09:19 +0100

    Changed config

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,7 +15,7 @@ module.exports = {
         // we want to force semicolons
         'semi': ['error', 'always'],
         // we use 2 spaces to indent our code
-        'indent': ['error', 2],
+        'indent': ['error', 2, { "SwitchCase": 1 }],
         // we want to avoid useless spaces
         'no-multi-spaces': ['error'],
         "vue/html-indent": "off",
