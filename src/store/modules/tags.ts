65ce4831591b920140be199b26d6641781c819ac
Jakob Bal <baljakob9@gmail.com>
2022-02-03 14:06:50 +0100

    Formatting and refactoring

--- src/store/modules/tags.ts
+++ src/store/modules/tags.ts
@@ -5,12 +5,20 @@ import {
   Mutation,
   Action,
   getModule
-} from 'vuex-module-decorators'
-import store from '@/store'
+} from 'vuex-module-decorators';
+import store from '@/store';
 
-import { Parameter, Job, JobList, LegendList, TagSelection, TagOrteResults } from '@/static/apiModels';
+import {
+  Parameter,
+  Job,
+  JobList,
+  LegendList,
+  TagSelection,
+  TagOrteResults,
+  SingleTag
+} from '@/static/apiModels';
 
-import { IGetPresetTagsResult } from "../../api/dioe-public-api/models/IGetPresetTagsResult";
+import { IGetPresetTagsResult } from '../../api/dioe-public-api/models/IGetPresetTagsResult';
 import { TagTree } from '@/api/dioe-public-api';
 import api from '@/api';
 import { getBerufe } from '@/api/erhebungen';
@@ -44,29 +52,29 @@ class Tags extends VuexModule implements TagState {
 
   get byUniqueURL() {
     return (name: string) => {
-      return this.legends.find(e  => e.name === name);
-    }
+      return this.legends.find(e => e.name === name);
+    };
   }
 
-  get tagOrteNum(){
+  get tagOrteNum() {
     return this.tagOrteResults;
   }
 
   get tags() {
-    return this.tagList ? this.tagList : [] as TagTree[];
+    return this.tagList ? this.tagList : ([] as TagTree[]);
   }
-  
+
   @Mutation
-  addParameter (parameter: Parameter) {
+  addParameter(parameter: Parameter) {
     this.parameters.push(parameter);
   }
-  
+
   @Mutation
-  addLegend (parameter: LegendList) {
+  addLegend(parameter: LegendList) {
     let currName = parameter.name;
     let i = 1;
-    while(this.legends.find(e  => e.name === currName) !== undefined){
-      currName = parameter.name + " " + i;
+    while (this.legends.find(e => e.name === currName) !== undefined) {
+      currName = parameter.name + ' ' + i;
       i += 1;
     }
     parameter.name = currName;
@@ -75,46 +83,50 @@ class Tags extends VuexModule implements TagState {
   }
 
   @Mutation
-  editLegendName(name: string, idx: number){
+  editLegendName(name: string, idx: number) {
     this.legends[idx].name = name;
   }
-  
+
   @Mutation
-  addParameterToLegend (idx: number, para: Parameter) {
+  addParameterToLegend(idx: number, para: Parameter) {
     const legend = this.legends[idx];
     legend.parameter.push(para);
-    this.legends[idx] = legend; 
+    this.legends[idx] = legend;
   }
 
   @Action
   changeLegendName(name: string, idx: number) {
-    if(idx > -1){
+    if (idx > -1) {
       const leg = this.legends[idx].name;
-      if(leg !== name) this.context.commit('editLegendName', {name: name, idx: idx});
+      if (leg !== name)
+        this.context.commit('editLegendName', { name: name, idx: idx });
     }
   }
 
   @Action
-  addLegendParameter(leg: any){
+  addLegendParameter(leg: any) {
     const legend = this.legends.find(e => e.name === leg.lName);
-    if(legend !== undefined){
+    if (legend !== undefined) {
       const idx = this.legends.indexOf(legend);
       // @ts-ignore
-      this.context.commit('addParameterToLegend', {idx: idx, para: parameter});
-    }else{
+      this.context.commit('addParameterToLegend', {
+        idx: idx,
+        para: {} as Parameter
+      });
+    } else {
       const parameters = [];
       parameters.push(leg.parameter);
-      let ls: LegendList = {} as LegendList;
+      const ls: LegendList = {} as LegendList;
       ls.name = leg.lname;
       ls.parameter = parameters;
       // @ts-ignore
       this.context.commit('addLegend', ls);
     }
   }
-  
+
   @Mutation
-  setTagList (ort: TagTree[] | null) {
-    this.tagList = ort
+  setTagList(ort: TagTree[] | null) {
+    this.tagList = ort;
   }
 
   @Mutation
@@ -129,117 +141,116 @@ class Tags extends VuexModule implements TagState {
 
   
   @Mutation
-  setJobList(list: Job[]){
-    this.jobList = list
-  }
-  
-  @Mutation
-  setParameters(list: Parameter[]){
-    this.parameters = list
+  setJobList(list: Job[]) {
+    this.jobList = list;
   }
 
   @Mutation
-  setLoading (loading: boolean) {
-    this.loading = loading
+  setParameters(list: Parameter[]) {
+    this.parameters = list;
+  }
+
+  @Mutation
+  setLoading(loading: boolean) {
+    this.loading = loading;
   }
 
   @Mutation
   clearLegend() {
     this.legends = [];
   }
-  
+
   @MutationAction({ mutate: ['tagList', 'loading'] })
-  async fetchTags () {
-    this.loading = true
-    console.log('trying to fetch data')
+  async fetchTags() {
+    this.loading = true;
+    console.log('trying to fetch data');
     const response = await api.dioePublic.getTags();
     console.log('fetched data');
     return {
       tagList: response,
       loading: false
-    }
+    };
   }
 
   @MutationAction({ mutate: ['tagList', 'loading'] })
-  async fetchGenTags (arg : {gen: number}) {
-    this.loading = true
-    console.log('trying to fetch data')
+  async fetchGenTags(arg: { gen: number }) {
+    this.loading = true;
+    console.log('trying to fetch data');
     const response = await api.dioePublic.getTagGen(arg.gen);
     console.log('fetched data');
     return {
       tagList: response,
       loading: false
-    }
+    };
   }
 
-  @MutationAction({mutate: ['tagOrteResults', 'loading']})
-  async fetchTagOrteResults (arg: {tagId: number}){
+  @MutationAction({ mutate: ['tagOrteResults', 'loading'] })
+  async fetchTagOrteResults(arg: { tagId: number }) {
     this.loading = true;
     const res = await api.dioePublic.getTagOrte(arg.tagId);
     return {
       tagOrteResults: res,
       loading: false
-    }
+    };
   }
 
-  @MutationAction({ mutate: ['tagOrteResults', 'loading']})
-  async fetchTagOrteResultsMultiple (arg: {ids: number[]}){
+  @MutationAction({ mutate: ['tagOrteResults', 'loading'] })
+  async fetchTagOrteResultsMultiple(arg: { ids: number[] }) {
     this.loading = true;
     const res = await api.dioePublic.getTagOrteMultiple(arg);
     return {
       tagOrteResults: res,
       loading: false
-    }
+    };
   }
 
-  
   @MutationAction({ mutate: ['jobList', 'loading'] })
-  async fetchJobs () {
-    this.loading = true
-    console.log('trying to fetch data')
+  async fetchJobs() {
+    this.loading = true;
+    console.log('trying to fetch data');
     const response = await getBerufe();
     console.log('fetched data');
     return {
       jobList: response.data,
       loading: false
-    }
+    };
   }
 
   @MutationAction({ mutate: ['presetTags', 'loading'] })
-  async fetchPresetTags () {
+  async fetchPresetTags() {
     // @ts-ignore
     this.commit('setLoading', true);
-    console.log('trying to fetch data')
+    console.log('trying to fetch data');
     const response = await api.dioePublic.getPresetTags();
     console.log('fetched data');
     return {
       presetTags: response,
       loading: false
-    }
+    };
   }
 
-  @MutationAction({ mutate: ['tagOrteResults', 'loading']})
-  async fetchTagOrtePreset (presetID: number){
+  @MutationAction({ mutate: ['tagOrteResults', 'loading'] })
+  async fetchTagOrtePreset(presetID: number) {
     this.loading = true;
-    const res = await api.dioePublic.getTagsFromPreset({ids: [presetID]});
+    const res = await api.dioePublic.getTagsFromPreset({ ids: [presetID] });
     return {
       tagOrteResults: res,
       loading: false
-    }
+    };
   }
 
-  @MutationAction({ mutate: ['tagOrteResults', 'loading']})
-  async fetchPresetTagOrte (presetID: number) {
+  @MutationAction({ mutate: ['tagOrteResults', 'loading'] })
+  async fetchPresetTagOrte(presetID: number) {
     // @ts-ignore
     this.commit('setLoading', true);
-    console.log('trying to fetch data')
+    console.log('trying to fetch data');
     const response = await api.dioePublic.getPresetOrte(presetID);
     console.log('fetched data');
     return {
       tagOrteResults: response,
       loading: false
-    }
+    };
   }
 }
 
-export const tagModule = getModule(Tags)
+export const tagModule = getModule(Tags);
