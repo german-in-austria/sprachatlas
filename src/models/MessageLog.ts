d0dcb0f49f31df228f63ae24f119a4b111e6c56e
Jakob Bal <baljakob9@gmail.com>
2022-02-15 12:09:32 +0100

    Added models for Message

---
+++ src/models/MessageLog.ts
@@ -0,0 +1,8 @@
+import { MessageType } from './MessageType';
+
+export interface MessageLog {
+  message: string;
+  time: Date;
+  icon: string;
+  type: MessageType;
+}
---
+++ src/models/MessageType.ts
@@ -0,0 +1,3 @@
+export enum MessageType {
+    info, error, success
+}
