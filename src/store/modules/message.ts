96ad00cb8e073302a471360ea26ee75cd824505a
Jakob Bal <baljakob9@gmail.com>
2022-02-15 15:46:04 +0100

    Added further support for messages

--- src/store/modules/message.ts
+++ src/store/modules/message.ts
@@ -7,14 +7,17 @@ import {
   getModule
 } from 'vuex-module-decorators';
 import store from '@/store';
+import { MessageLog } from '@/models/MessageLog';
+import { SimpleMsg } from '@/models/SimpleMsg';
+import { MessageType } from '@/models/MessageType';
+
+import { SNACK_BAR_TIMEOUT } from '@/constants/index';
+import { debounce } from 'lodash';
 
 interface MessageState {
-  errorMsg: string;
-  successMsg: string;
-  warnMsg: string;
-  showError: boolean;
-  showSuccess: boolean;
-  showWarn: boolean;
+  showSnackbar: boolean;
+  messageQueue: Array<MessageLog>;
+  activationTime: Date | null;
 }
 
 @Module({
@@ -23,6 +26,9 @@ interface MessageState {
   dynamic: true
 })
 class Message extends VuexModule implements MessageState {
+  messageQueue: Array<MessageLog> = [];
+  activationTime: Date | null = null;
+  showSnackbar: boolean = false;
   errorMsg: string = '';
   showError: boolean = false;
   successMsg: string = '';
@@ -30,34 +36,89 @@ class Message extends VuexModule implements MessageState {
   warnMsg: string = '';
   showWarn: boolean = false;
 
-  @Mutation
-  setErrorMsg(msg: string) {
-    this.errorMsg = msg;
+  get queue() {
+    return this.messageQueue;
+  }
+
+  get showSnack() {
+    return this.showSnackbar;
+  }
+
+  get activation() {
+    return this.activationTime;
   }
 
   @Mutation
-  setWarnMsg(msg: string) {
-    this.warnMsg = msg;
+  setShow(shw: boolean) {
+    this.showSnackbar = shw;
   }
 
   @Mutation
-  setSuccessMsg(msg: string) {
-    this.successMsg = msg;
+  closeMsg(time: number){
+    if(time === 0) return;
+    const ele = this.messageQueue.find((el) => el.time.getTime() === time);
   }
 
   @Mutation
-  displayError() {
-    this.showError = !this.showError;
+  addMessage(msg: MessageLog){
+    const now = new Date();
+    const newMsg: MessageLog = msg;
+    if(!msg.time){
+      newMsg.time = now;
+    }
+
+    this.messageQueue.push(newMsg);
+    this.activationTime = now;
+    this.showSnackbar = true;
   }
 
-  @Mutation
-  displaySuccess() {
-    this.showSuccess = !this.showSuccess;
+  @Action
+  showDebounce() {
+    const msgDebounce = debounce(() => {
+      if(this.activationTime && Date.now() - this.activationTime.getTime() >= SNACK_BAR_TIMEOUT)
+        this.context.commit('setShow', false);
+    }, SNACK_BAR_TIMEOUT);
+    msgDebounce();
   }
 
-  @Mutation
-  displayWarn() {
-    this.showWarn = !this.showWarn;
+  @Action
+  setMessage(msg: MessageLog) {
+    this.context.commit('addMessage', msg);
+    debounce(() => {
+      if(this.activationTime && Date.now() - this.activationTime.getTime() >= SNACK_BAR_TIMEOUT)
+        this.context.commit('setShow', false);
+    }, 1000);
+    this.context.dispatch('showDebounce');
+  }
+
+  @Action
+  setErrorMsg(msg: SimpleMsg) {
+    this.context.dispatch('setMessage', {
+      message: msg.message,
+      icon: msg.icon,
+      time: Date.now(),
+      type: MessageType.error
+    });
+  }
+
+  @Action
+  setWarnMsg(msg: SimpleMsg) {
+    this.context.dispatch('setMessage', {
+      message: msg.message,
+      icon: msg.icon,
+      time: Date.now(),
+      type: MessageType.info
+    });
+  }
+
+  @Action
+  setSuccessMsg(msg: SimpleMsg) {
+    this.context.dispatch('setMessage', {
+      message: msg.message,
+      icon: msg.icon,
+      time: Date.now(),
+      type: MessageType.success
+    });
   }
   
 }
