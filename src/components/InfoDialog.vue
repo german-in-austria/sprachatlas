4dca0114cf1f5f126492c9d91bc4b9bef9cf4d96
Jakob Bal <baljakob9@gmail.com>
2022-02-24 17:25:30 +0100

    created component InfoDialog

---
+++ src/components/InfoDialog.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-dialog> </v-dialog>
+</template>
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component({
+  // if you use components add them here
+  components: {},
+  /* name is necessary for recursive components
+   * (at least in older versions, might be auto generated through the vue-property-decorator)
+   */
+  name: 'InfoDialog'
+})
+export default class InfoDialog extends Vue {}
+</script>
