4a21615a1a37729847e75e4a6b616e2a54505a3d
Jakob Bal <baljakob9@gmail.com>
2022-03-07 17:59:45 +0100

    Implemented basic version of age slider

--- src/components/AgeRange.vue
+++ src/components/AgeRange.vue
@@ -1,11 +1,29 @@
 <template>
-  <div>
-    <v-menu :close-on-content-click="false" :nudge-width="800" offset-y>
+  <div class="text-center">
+    <v-menu
+      v-model="menu"
+      :close-on-content-click="false"
+      :nudge-width="800"
+      offset-y
+    >
       <template v-slot:activator="{ on, attrs }">
         <v-btn class="mx-1" fab small v-bind="attrs" v-on="on">
           <v-icon> mdi-av-timer </v-icon>
         </v-btn>
       </template>
+      <v-card>
+        <v-list>
+          <v-list-item-title> Alter einstellen </v-list-item-title>
+          <v-list-item-content>
+            <v-range-slider
+              hint="Alter einstellen"
+              min="0"
+              max="100"
+              v-model="ages"
+            ></v-range-slider>
+          </v-list-item-content>
+        </v-list>
+      </v-card>
     </v-menu>
   </div>
 </template>
@@ -17,5 +35,8 @@ import { Prop, Vue } from 'vue-property-decorator';
   name: 'AgeRange',
   components: {}
 })
-export default class AgeRange extends Vue {}
+export default class AgeRange extends Vue {
+  menu = false;
+  ages = [20, 70];
+}
 </script>
