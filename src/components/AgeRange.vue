f73fd38e225bfe74cf17d535b96eb96774508e5e
Jakob Bal <baljakob9@gmail.com>
2022-03-09 09:15:22 +0100

    styling ageFilter Component

--- src/components/AgeRange.vue
+++ src/components/AgeRange.vue
@@ -3,26 +3,78 @@
     <v-menu
       v-model="menu"
       :close-on-content-click="false"
-      :nudge-width="800"
+      :nudge-width="500"
       offset-y
     >
-      <template v-slot:activator="{ on, attrs }">
-        <v-btn class="mx-1" fab small v-bind="attrs" v-on="on">
-          <v-icon> mdi-av-timer </v-icon>
-        </v-btn>
+      <template v-slot:activator="{ on: menu, attrs }">
+        <v-tooltip bottom>
+          <template v-slot:activator="{ on: tooltip }">
+            <v-btn
+              class="mx-1"
+              fab
+              small
+              v-bind="attrs"
+              v-on="{ ...tooltip, ...menu }"
+            >
+              <v-icon> mdi-av-timer </v-icon>
+            </v-btn>
+          </template>
+          <span>Altersgrenzen in Jahren einstellen</span>
+        </v-tooltip>
       </template>
       <v-card>
         <v-list>
-          <v-list-item-title> Alter einstellen </v-list-item-title>
-          <v-list-item-content>
-            <v-range-slider
-              hint="Alter einstellen"
-              min="0"
-              max="100"
-              v-model="ages"
-            ></v-range-slider>
-          </v-list-item-content>
+          <v-list-item>
+            <v-list-item-content>
+              <v-list-item-title>
+                <h3>Alter in Jahren einstellen</h3>
+              </v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+          <v-list-item>
+            <v-divider></v-divider>
+          </v-list-item>
+          <v-list-item>
+            <v-list-item-content>
+              <v-range-slider
+                hint="Alter (Jahre) einstellen"
+                min="0"
+                max="100"
+                v-model="ages"
+                ><template v-slot:prepend>
+                  <v-text-field
+                    :value="ages[0]"
+                    class="mt-0 pt-0"
+                    hide-details
+                    single-line
+                    type="number"
+                    style="width: 60px"
+                    @change="$set(range, 0, $event)"
+                  ></v-text-field>
+                </template>
+                <template v-slot:append>
+                  <v-text-field
+                    :value="ages[1]"
+                    class="mt-0 pt-0"
+                    hide-details
+                    single-line
+                    type="number"
+                    style="width: 60px"
+                    @change="$set(range, 1, $event)"
+                  ></v-text-field>
+                </template>
+              </v-range-slider>
+            </v-list-item-content>
+          </v-list-item>
         </v-list>
+        <v-card-actions class="pa-5">
+          <v-btn class="mr-10" fab small color="error"
+            ><v-icon>mdi-arrow-u-left-top</v-icon></v-btn
+          >
+          <v-btn fab small color="success"
+            ><v-icon>mdi-filter-outline</v-icon></v-btn
+          >
+        </v-card-actions>
       </v-card>
     </v-menu>
   </div>
