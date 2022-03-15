3dda52149fc609d1d085496666b452f3da4f4544
Jakob Bal <baljakob9@gmail.com>
2022-03-15 16:37:23 +0100

    Created new component

---
+++ src/components/ErhebungsArt.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="text-center"></div>
+</template>
+<script lang="ts">
+import Component from 'vue-class-component';
+import { Prop, Vue } from 'vue-property-decorator';
+import { legendMod } from '@/store/modules/legend';
+import { messageHandler } from '@/store/modules/message';
+
+@Component({
+  name: 'ErhebungsArt',
+  components: {}
+})
+export default class ErhebungsArt extends Vue {}
+</script>
