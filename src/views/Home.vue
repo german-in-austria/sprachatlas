e31acd82039dd78be1e7525ba8032b42836b272e
Jakob Bal <baljakob9@gmail.com>
2022-04-06 11:07:54 +0200

    formatting

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,15 +5,15 @@
 </template>
 
 <script lang="ts">
-import { Component, Vue } from "vue-property-decorator";
-import MapView from "@/components/MapView.vue"; // @ is an alias to /src
-import QueryCreator from "@/components/QueryCreator.vue";
+import { Component, Vue } from 'vue-property-decorator';
+import MapView from '@/components/MapView.vue'; // @ is an alias to /src
+import QueryCreator from '@/components/QueryCreator.vue';
 
 @Component({
   components: {
     MapView,
-    QueryCreator,
-  },
+    QueryCreator
+  }
 })
 export default class Home extends Vue {}
 </script>
