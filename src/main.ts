658bde9139739a8ebc8463c038ad45bfcc111f56
Jakob Bal <baljakob9@gmail.com>
2022-04-06 13:07:47 +0200

    formatting

--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,23 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
-import 'leaflet/dist/leaflet.css'
-import vuetify from './plugins/vuetify'
-import { Icon } from 'leaflet'
+import Vue from 'vue';
+import App from './App.vue';
+import router from './router';
+import store from './store';
+import 'leaflet/dist/leaflet.css';
+import vuetify from './plugins/vuetify';
+import { Icon } from 'leaflet';
 
 type D = Icon.Default & {
   _getIconUrl?: string;
 }
-delete (Icon.Default.prototype as D)._getIconUrl
+delete (Icon.Default.prototype as D)._getIconUrl;
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 const vm = new Vue({
   router,
   store,
   vuetify,
   render: h => h(App)
-}).$mount('#app')
+}).$mount('#app');
 
-export default vm
+export default vm;
