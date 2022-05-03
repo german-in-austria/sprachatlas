1083f9479c3d173a64ef440252900642e7cc3511
Jakob Bal <baljakob9@gmail.com>
2022-05-03 13:00:57 +0200

    refactoring

--- src/components/SymbolPicker.vue
+++ src/components/SymbolPicker.vue
@@ -25,17 +25,20 @@
   </v-select>
 </template>
 <script lang="ts">
-import { Component, PropSync, Vue, Prop, Watch } from "vue-property-decorator";
-import { drawRect, drawTriangle } from "@/helpers/MapCompute";
-import { Symbols } from "@/static/apiModels";
-import IconCircle from "@/icons/IconCircle.vue";
+import { Component, PropSync, Vue, Prop, Watch } from 'vue-property-decorator';
+import { drawRect, drawTriangle } from '@/helpers/MapCompute';
+import { Symbols } from '@/static/apiModels';
+import IconCircle from '@/icons/IconCircle.vue';
 
 @Component({
   components: { IconCircle },
-  name: "SymbolPicker",
+  name: 'SymbolPicker'
 })
 export default class SymbolPicker extends Vue {
-  @Prop({ default: "#F00", type: String }) readonly color!: string;
+  @Prop({ default: '#F00', type: String }) readonly color!: string;
+  @Prop({ default: 0, type: Number, required: false })
+  readonly propSymbol!: number;
+
   selSymbol: Symbols = Symbols.Circle;
 
   get symbol() {
@@ -50,10 +53,16 @@ export default class SymbolPicker extends Vue {
     return drawTriangle(size, border, color, encoded, 1.0);
   }
 
+  created() {
+    if (this.propSymbol) {
+      this.selSymbol = this.propSymbol;
+    }
+  }
+
   symbols = [
-    { name: "Kreis", value: Symbols.Circle },
-    { name: "Rechteck", value: Symbols.Rect },
-    { name: "Dreieck", value: Symbols.Tri },
+    { name: 'Kreis', value: Symbols.Circle },
+    { name: 'Rechteck', value: Symbols.Rect },
+    { name: 'Dreieck', value: Symbols.Tri }
   ];
 }
 </script>
