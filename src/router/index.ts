3accbbe4881c17e21d9398030ce93f53b1828403
Jakob Bal <baljakob9@gmail.com>
2022-06-21 16:07:49 +0200

    Minor formatting

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,14 +1,13 @@
-import Vue from 'vue'
-import VueRouter, { RouteConfig } from 'vue-router'
-import Home from '../views/Home.vue'
-import MapView from '../components/MapView.vue'
-import QueryCreator from '../components/QueryCreator.vue'
-import TagView from '@/components/TagView.vue'
+import Vue from 'vue';
+import VueRouter, { RouteConfig } from 'vue-router';
+import Home from '../views/Home.vue';
+import MapView from '../components/MapView.vue';
+import QueryCreator from '../components/QueryCreator.vue';
+import TagView from '@/components/TagView.vue';
 
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 
 const routes: Array<RouteConfig> = [
-  
   {
     name: 'home',
     path: '/',
@@ -28,20 +27,22 @@ const routes: Array<RouteConfig> = [
         path: 'tags',
         name: 'TagSelection',
         component: TagView
-      },
-    ]},
+      }
+    ]
+  },
   {
     path: '/about',
     name: 'About',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    component: () =>
+      import(/* webpackChunkName: "about" */ '../views/About.vue')
   }
-]
+];
 
 const router = new VueRouter({
   routes
-})
+});
 
-export default router
+export default router;
