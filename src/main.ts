4eaa7e518569c2b37cfcf0d484a76fd04bf47db3
Jakob Bal <baljakob9@gmail.com>
2022-07-21 11:54:12 +0200

    Formatting

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ import { Icon } from 'leaflet';
 
 type D = Icon.Default & {
   _getIconUrl?: string;
-}
+};
 delete (Icon.Default.prototype as D)._getIconUrl;
 
 Vue.config.productionTip = false;
@@ -17,7 +17,7 @@ const vm = new Vue({
   router,
   store,
   vuetify,
-  render: h => h(App)
+  render: (h) => h(App)
 }).$mount('#app');
 
 export default vm;
