91f4916cb95ae77c056b5e69e5b968234ea7bd59
Jakob Bal <baljakob9@gmail.com>
2022-08-08 11:37:46 +0200

    Formatting

--- src/helpers/MapCompute.ts
+++ src/helpers/MapCompute.ts
@@ -1,10 +1,13 @@
 import { TagOrteResults } from '../static/apiModels';
 
-export const computePropCircle = (data: Array<number>, maxRadius: number): number => {
+export const computePropCircle = (
+  data: Array<number>,
+  maxRadius: number
+): number => {
   const radiusOfData: Array<number> = [];
   let max = 0;
   for (const d of data) {
-    if(d > max) max = d;
+    if (d > max) max = d;
   }
   return maxRadius / max;
 };
@@ -54,35 +57,126 @@ export const describeArc = (
   return d;
 };
 
-export const drawCircleDiagram = (size: number, border: number, borderColor: string, color: string, data: [{v: number, c: string}], encoded: boolean) => {
+export const drawCircleDiagram = (
+  size: number,
+  border: number,
+  borderColor: string,
+  color: string,
+  data: [{ v: number; c: string }],
+  encoded: boolean
+) => {
   const hSize = size * 0.5;
   const ihSize = (size - border * 2) * 0.5;
   let out = '';
-  out += '<circle cx="' + hSize + '" cy="' + hSize + '" r="' + hSize + '" fill="' + borderColor + '" />';
-  out += '<circle cx="' + hSize + '" cy="' + hSize + '" r="' + ihSize + '" fill="' + color + '" />';
-  const max: number = data.flat(Infinity).reduce((a: any, b: any) => Number(a) + (Number(b.v) || 0), 0);
+  out +=
+    '<circle cx="' +
+    hSize +
+    '" cy="' +
+    hSize +
+    '" r="' +
+    hSize +
+    '" fill="' +
+    borderColor +
+    '" />';
+  out +=
+    '<circle cx="' +
+    hSize +
+    '" cy="' +
+    hSize +
+    '" r="' +
+    ihSize +
+    '" fill="' +
+    color +
+    '" />';
+  const max: number = data
+    .flat(Infinity)
+    .reduce((a: any, b: any) => Number(a) + (Number(b.v) || 0), 0);
   const angMulti = 360 / max;
   let lAng = 0;
   data.forEach((el: any) => {
     const nAng = lAng + el.v * angMulti;
-    out += '<path d="' + describeArc(hSize, hSize, ihSize, lAng, nAng) + '" stroke-width="0" fill="' + (el.c || '#f00') + '" />';
+    out +=
+      '<path d="' +
+      describeArc(hSize, hSize, ihSize, lAng, nAng) +
+      '" stroke-width="0" fill="' +
+      (el.c || '#f00') +
+      '" />';
     lAng = nAng;
   });
-  out = '<?xml version="1.0" encoding="UTF-8"?><!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd"><svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1"  width="' + size + '" height="' + size + '" viewBox="0 0 ' + size + ' ' + size + '">' + out + '</svg>';
-  return encoded ? 'data:image/svg+xml;base64,' + window.btoa(unescape(encodeURIComponent(out))) : out;
+  out =
+    '<?xml version="1.0" encoding="UTF-8"?><!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd"><svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1"  width="' +
+    size +
+    '" height="' +
+    size +
+    '" viewBox="0 0 ' +
+    size +
+    ' ' +
+    size +
+    '">' +
+    out +
+    '</svg>';
+  return encoded
+    ? 'data:image/svg+xml;base64,' +
+        window.btoa(unescape(encodeURIComponent(out)))
+    : out;
 };
 
-export const drawRect = (size: number, border: number, color: string, encoded: boolean, opacity: number) => {
+export const drawRect = (
+  size: number,
+  border: number,
+  color: string,
+  encoded: boolean,
+  opacity: number
+) => {
   const hSize = size * 1.7;
   let out = '';
   out += `<rect x="0" y="0" width="${size}" height="${hSize}" fill="${color}" fill-opacity="${opacity}" stroke="${color}" stroke-opacity="0.8" stroke-width="${border}"/>`;
-  out = '<?xml version="1.0" encoding="UTF-8"?><!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd"><svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1"  width="' + size + '" height="' + hSize + '" viewBox="0 0 ' + size + ' ' + hSize + '">' + out + '</svg>';
-  return encoded ? 'data:image/svg+xml;base64,' + window.btoa(unescape(encodeURIComponent(out))) : out;
+  out =
+    '<?xml version="1.0" encoding="UTF-8"?><!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd"><svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1"  width="' +
+    size +
+    '" height="' +
+    hSize +
+    '" viewBox="0 0 ' +
+    size +
+    ' ' +
+    hSize +
+    '">' +
+    out +
+    '</svg>';
+  return encoded
+    ? 'data:image/svg+xml;base64,' +
+        window.btoa(unescape(encodeURIComponent(out)))
+    : out;
 };
 
-export const drawTriangle = (size: number, border: number, color: string, encoded: boolean, opacity: number) => {
+export const drawTriangle = (
+  size: number,
+  border: number,
+  color: string,
+  encoded: boolean,
+  opacity: number
+) => {
   let out = '';
-  out += `<polygon points="${size/2},0 ${size},${size} 0,${size}" fill="${color}" fill-opacity="${opacity}" stroke="${color}" stroke-opacity="0.8" stroke-width="${border / 10}"/>`;
-  out = '<?xml version="1.0" encoding="UTF-8"?><!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd"><svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1"  width="' + size + '" height="' + size + '" viewBox="0 0 ' + size + ' ' + size + '">' + out + '</svg>';
-  return encoded ? 'data:image/svg+xml;base64,' + window.btoa(unescape(encodeURIComponent(out))) : out;
+  console.log(color);
+  out += `<polygon points="${
+    size / 2
+  },0 ${size},${size} 0,${size}" fill="${color}" fill-opacity="${opacity}" stroke="${color}" stroke-opacity="0.8" stroke-width="${
+    border / 10
+  }"/>`;
+  out =
+    '<?xml version="1.0" encoding="UTF-8"?><!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd"><svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1"  width="' +
+    size +
+    '" height="' +
+    size +
+    '" viewBox="0 0 ' +
+    size +
+    ' ' +
+    size +
+    '">' +
+    out +
+    '</svg>';
+  return encoded
+    ? 'data:image/svg+xml;base64,' +
+        window.btoa(unescape(encodeURIComponent(out)))
+    : out;
 };
