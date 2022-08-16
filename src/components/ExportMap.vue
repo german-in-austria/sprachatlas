c5e1848fcbf5f84bb1479f316aa453e5ddd8bb25
Jakob Bal <baljakob9@gmail.com>
2022-08-16 12:13:53 +0200

    Added component exportMap

---
+++ src/components/ExportMap.vue
@@ -0,0 +1,15 @@
+<template></template>
+<script lang="ts">
+import { Prop, Vue, Component } from 'vue-property-decorator';
+
+
+@Component({
+  name: 'ExportMap',
+  components: {}
+})
+export default class ExportMap extends Vue {
+  get url() {
+    return window.location.href;
+  }
+}
+</script>
