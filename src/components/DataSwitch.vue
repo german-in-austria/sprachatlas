c52650a60ab8b3dbefc8182fcdfcc59d186a9977
Jakob Bal <baljakob9@gmail.com>
2022-08-17 14:44:45 +0200

    Created data switch component

---
+++ src/components/DataSwitch.vue
@@ -0,0 +1,60 @@
+<template>
+  <v-container>
+    <v-row align="center" justify="center">
+      <v-btn icon color="indigo" @click="changeData(false)">
+        <v-icon>mdi-arrow-left-bold</v-icon>
+      </v-btn>
+      <template v-if="data.icon === 0 || !sideways">
+        <v-avatar>
+          <icon-circle :fillCol="data.c" :strokeWidth="1" />
+        </v-avatar>
+      </template>
+      <template v-else-if="data.icon === 1">
+        <img :src="drawRect(20, 1, data.c, true)" />
+      </template>
+      <template v-else>
+        <img :src="drawTriangle(20, 1, data.c, true)" />
+      </template>
+      {{ data.name }} - Verfügbare Audioaufnahmen für
+      {{ ortName }}
+      <v-btn icon color="indigo" @click="changeData(true)">
+        <v-icon>mdi-arrow-right-bold</v-icon>
+      </v-btn>
+    </v-row>
+  </v-container>
+</template>
+<script lang="ts">
+import { Prop, Vue, Component } from 'vue-property-decorator';
+import IconCircle from '@/icons/IconCircle.vue';
+
+import { drawRect, drawTriangle } from '@/helpers/MapCompute';
+
+
+import { Hsl, singleEntry } from '@/static/apiModels';
+
+@Component({
+  components: {
+    IconCircle
+  },
+  name: 'DataSwitch'
+})
+export default class DataSwitch extends Vue {
+  @Prop(String) readonly ortName!: string;
+  @Prop({ type: String, default: "" }) readonly typeLeg!: string;
+  @Prop(Boolean) readonly sideways!: boolean;
+  @Prop() readonly data!: singleEntry;
+
+
+  changeData(dir: boolean) {
+    this.$emit('callChange', dir);
+  }
+
+  drawRect(size: number, border: number, color: string, encoded: boolean) {
+    return drawRect(size, border, color, encoded, 1.0);
+  }
+
+  drawTriangle(size: number, border: number, color: string, encoded: boolean) {
+    return drawTriangle(size, border, color, encoded, 1.0);
+  }
+}
+</script>
