335395720f5cd04ebe2bb7e47e1d91b75bd32bb4
Jakob Bal <baljakob9@gmail.com>
2022-08-23 15:45:32 +0200

    Updated style

--- src/components/TokenField.vue
+++ src/components/TokenField.vue
@@ -1,27 +1,29 @@
 <template>
-  <v-text-field
-    v-model="inputModel"
-    :label="label"
-    clearable
-    :hint="hint"
-    append-outer-icon="mdi-plus"
-    @click:append-outer="addToken"
-    @keydown.enter="addToken"
-  ></v-text-field>
-  <v-chip
-    v-for="(val, idx) in textToken"
-    :key="idx"
-    class="ma-2"
-    close
-    color="orange"
-    text-color="white"
-    @click:close="textToken.splice(idx, 1)"
-  >
-    {{ val }}
-  </v-chip>
+  <fieldset class="box">
+    <legend>{{ header }}</legend>
+    <v-text-field
+      v-model="inputModel"
+      :label="label"
+      clearable
+      :hint="hint"
+      :append-outer-icon="appendIcon"
+      @click:append-outer="addToken"
+      @keydown.enter="addToken"
+    ></v-text-field>
+    <v-chip
+      v-for="(val, idx) in textToken"
+      :key="idx"
+      class="ma-2"
+      close
+      :color="color"
+      text-color="white"
+      @click:close="textToken.splice(idx, 1)"
+    >
+      {{ val }}
+    </v-chip>
+  </fieldset>
 </template>
 <script lang="ts">
-import { readonly } from 'vue';
 import { Prop, Vue, Component } from 'vue-property-decorator';
 import { expData } from '@/service/ExportBase';
 import { SearchItems } from '@/static/apiModels';
@@ -35,7 +37,11 @@ import { messageHandler } from '@/store/modules/message';
 })
 export default class TokenField extends Vue {
   @Prop({ type: String, default: 'Anfrage eingeben' }) readonly label!: string;
+  @Prop({ type: String, default: '' }) readonly header!: string;
   @Prop({ type: String, default: '' }) readonly hint!: string;
+  @Prop({ type: String, default: 'mdi-plus' }) readonly appendIcon!: string;
+  @Prop({ type: String, default: '#FF0000' }) readonly color!: string;
+
 
   inputModel: string = "";
   textToken: string[] = [];
@@ -43,6 +49,26 @@ export default class TokenField extends Vue {
   addToken() {
     this.textToken.push(this.inputModel);
     this.inputModel = "";
+    this.$emit('update:elements', this.textToken);
+  }
+
+  mounted() {
+
   }
 }
 </script>
+<style scoped>
+  .box {
+    border: 1px solid black;
+    border-radius: 10px;
+    box-sizing: border-box;
+    grid-area: 1 / 2;
+    padding: 20px;
+  }
+
+  .box legend {
+    margin-right: auto;
+    padding: 0.2em 1.5em;
+    border-bottom-left-radius: 1em;
+  }
+</style>
