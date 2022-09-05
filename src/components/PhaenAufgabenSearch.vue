3a87c4f90c2da0f4b24661a321a2e577c609f736
Jakob Bal <baljakob9@gmail.com>
2022-09-05 09:43:20 +0200

    Created new component

---
+++ src/components/PhaenAufgabenSearch.vue
@@ -0,0 +1,12 @@
+<template></template>
+<script lang="ts">
+import { Component, PropSync, Vue, Prop, Watch } from 'vue-property-decorator';
+
+@Component({
+  components: {},
+  name: 'PhaenAufgabenSearch'
+})
+export default class PhaenAufgabenSearch extends Vue {
+
+}
+</script>
