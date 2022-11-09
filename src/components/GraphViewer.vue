e150e97f45a66f0c9d6d1eb02756e569c0e87ed1
Jakob Bal <baljakob9@gmail.com>
2022-11-09 11:27:53 +0100

    #34: created comp GraphViewer

---
+++ src/components/GraphViewer.vue
@@ -0,0 +1,18 @@
+<template></template>
+<script lang="ts">
+import { Component, PropSync, Vue, Prop, Watch } from 'vue-property-decorator';
+
+export interface Description {
+  color: string;
+  name: string;
+  value: number;
+}
+
+@Component({
+  components: {},
+  name: 'GraphViewer'
+})
+export default class GraphViewer extends Vue {
+  @Prop({ type: Array, required: false }) readonly desc!: Array<Description>;
+}
+</script>
