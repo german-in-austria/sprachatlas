2efd42b03aacd5409322317ea15284327e8c452b
Jakob Bal <baljakob9@gmail.com>
2022-11-17 15:59:12 +0100

    Created CircleDiagramm component

---
+++ src/components/CircleDiagram.vue
@@ -0,0 +1,13 @@
+<template>
+  <div v-if="code.length > 0" v-html="code"></div>
+</template>
+<script lang="ts">
+import { Component, PropSync, Vue, Prop, Watch } from 'vue-property-decorator';
+
+@Component({
+  components: {},
+  name: 'CircleDiagram'
+})
+export default class CircleDiagram extends Vue {
+  @Prop(String) readonly code!: string;
+}
