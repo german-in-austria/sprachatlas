1a612f884c76fab878132fccd4c504f988a89836
Jakob Bal <baljakob9@gmail.com>
2022-11-24 10:54:44 +0100

    Created component variationCard

---
+++ src/components/VariationCard.vue
@@ -0,0 +1,13 @@
+<template></template>
+<script lang="ts">
+import { Component, PropSync, Vue, Prop, Watch } from 'vue-property-decorator';
+import { Description, Symbols } from '@/static/apiModels';
+import GraphViewer from './GraphViewer.vue';
+
+@Component({
+  components: { GraphViewer },
+  name: 'VariationCard'
+})
+export default class VariationCard extends Vue {
+}
+</script>
