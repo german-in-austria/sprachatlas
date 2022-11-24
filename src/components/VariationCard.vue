0af2f5ea47b5d3316440f6b2030863c62d87d2c6
Jakob Bal <baljakob9@gmail.com>
2022-11-24 12:51:11 +0100

    Added code and style for card

--- src/components/VariationCard.vue
+++ src/components/VariationCard.vue
@@ -1,4 +1,9 @@
-<template></template>
+<template>
+  <v-card class="card" v-if="desc.length > 0">
+    <v-card-title>{{ title }}</v-card-title>
+    <v-card-text> <graph-viewer :desc="desc" /> </v-card-text>
+  </v-card>
+</template>
 <script lang="ts">
 import { Component, PropSync, Vue, Prop, Watch } from 'vue-property-decorator';
 import { Description, Symbols } from '@/static/apiModels';
@@ -9,5 +14,19 @@ import GraphViewer from './GraphViewer.vue';
   name: 'VariationCard'
 })
 export default class VariationCard extends Vue {
+  @Prop(String) readonly title!: string;
+  @Prop({ type: Array }) readonly desc!: Array<Description>;
+
 }
 </script>
+<style lang="scss">
+  .v-card__text {
+    flex-grow: 1;
+    overflow: auto;
+    overflow-y: scroll;
+  }
+
+  .card {
+    width: 450px;
+  }
+</style>
