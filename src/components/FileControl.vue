c0c8ebdf118659012e5b7551e915112f536ab1eb
Jakob Bal <baljakob9@gmail.com>
2023-01-04 15:42:02 +0100

    Created comp FileControl for switching

---
+++ src/components/FileControl.vue
@@ -0,0 +1,12 @@
+<template></template>
+<script lang="ts">
+import { Component, Vue, Prop, PropSync } from 'vue-property-decorator';
+
+@Component({
+  components: {},
+  name: 'FileControl'
+})
+export default class FileControl extends Vue {
+  @PropSync("dataIdx") readonly dataIdx!: number;
+}
+</script>
