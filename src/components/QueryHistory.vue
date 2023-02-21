33c6d0facf077c750943bf47db9b405b8ea11e85
Jakob Bal <baljakob9@gmail.com>
2023-02-21 11:16:45 +0100

    Created component QueryHistory

---
+++ src/components/QueryHistory.vue
@@ -0,0 +1,15 @@
+<template></template>
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+import { messageHandler } from '@/store/modules/message';
+
+import { legendMod } from '@/store/modules/legend';
+import { SearchItems } from '@/static/apiModels';
+
+@Component({
+  components: {},
+  name: 'QueryHistory'
+})
+export default class QueryHistory extends Vue {
+}
+</script>
