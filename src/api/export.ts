0aceb63817b23427ebc79502e56fbf9d96d0a402
Jakob Bal <baljakob9@gmail.com>
2023-02-23 14:18:37 +0100

    Added positive response for export

--- src/api/export.ts
+++ src/api/export.ts
@@ -7,12 +7,14 @@ export const getSingleExportLink = (
   return api.dioeDB.get(`/restapi/get?=${id}`);
 };
 
-export const postNewExportLink = (
-  data: string
-): Promise<AxiosResponse<any>> => {
-  return api.dioeDB.post('', {
-    data: data
-  });
+export const postNewExportLink = async (data: string): Promise<any> => {
+  return await api.dioeDB
+    .post('/restapi/post', {
+      data: data
+    })
+    .then((response) => {
+      return { status: response.status, data: response.data };
+    });
 };
 
 export const deleteSingleExportLink = (
