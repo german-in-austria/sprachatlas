17e8c0dfd10de7a0e096fde1292633f3b2b9ac83
Jakob Bal <baljakob9@gmail.com>
2023-08-01 15:37:22 +0200

    Added sorting by sigle

--- src/components/KontextView.vue
+++ src/components/KontextView.vue
@@ -1,9 +1,16 @@
 <template>
-  <div>
-    <div class="token ma-2" v-for="(d, idx) in kontextData" :key="idx">
-      {{ d.ortho }} / {{ d.sppos }} / {{ d.sigle }}
-    </div>
-  </div>
+  <span>
+    <span v-for="(sig, i) in sigle" :key="i">
+      <span
+        class="token mb-2 mr-2"
+        v-for="(d, idx) in kontextData.filter((el) => el.sigle === sig)"
+        :key="idx"
+      >
+        {{ d.ortho }} / {{ d.sppos }} / {{ d.sigle }}
+      </span>
+      <br />
+    </span>
+  </span>
 </template>
 <script lang="ts">
 import { AntwortKontext } from '@/api/dioe-public-api';
@@ -15,8 +22,11 @@ import { Component, Vue, Prop } from 'vue-property-decorator';
 })
 export default class KontextView extends Vue {
   @Prop() readonly kontextData!: AntwortKontext[];
+  sigle = null as any;
 
-  mounted() {}
+  mounted() {
+    this.sigle = new Set(this.kontextData.map((el) => el.sigle));
+  }
 }
 </script>
 <style lang="scss">
