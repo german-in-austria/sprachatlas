5684628cb7733e1fcc7d3f7f083a900c574d38c4
Jakob Bal <baljakob9@gmail.com>
2023-08-02 12:21:39 +0200

    Created chart viewer component

---
+++ src/components/ChartViewer.vue
@@ -0,0 +1,23 @@
+<template>
+  <svg></svg>
+</template>
+<script lang="ts">
+import { Component, PropSync, Vue, Prop, Watch } from 'vue-property-decorator';
+
+import * as d3 from 'd3';
+
+@Component({
+  components: {},
+  name: 'ChartViewer'
+})
+export default class ChartViewer extends Vue {
+  @Prop() readonly inputData!: Array<any>;
+
+  mounted() {
+    const width = 800;
+    const height = 500;
+    const svg = d3.select('svg').attr('width', width).attr('height', height);
+    const g = svg.append('g');
+  }
+}
+</script>
